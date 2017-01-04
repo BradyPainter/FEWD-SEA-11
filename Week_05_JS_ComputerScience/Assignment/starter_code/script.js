1af90fce4d09e434ebcb8e14a06c1ce3894cbc37
Brady <Braden.Painter@gmail.com>
2017-01-04 13:21:48 -0800

    Final changes

---
+++ Week_05_JS_ComputerScience/Assignment/starter_code/script.js
@@ -0,0 +1,24 @@
+/*
+I tried to figure out what the error was for 1st line (actual 6th line) 
+it said $ is not defind.
+*/
+
+var city = $('#city-type').val();
+
+$('#submit-btn').click("submit");
+
+if (city === "New York" || "New York City" || "NYC") {
+	$('body').css('.nyc');
+	} 
+	else if (city === "San Francisco" || "SF" || "Bay Area") {
+		$('body').css('.sf');
+	}
+	else if (city === "Los Angeles" || "LA" || "LAX") {
+		$('body').css('.la');
+	}
+	else if (city === "Austin" || "ATX") {
+		$('body').css('.austin');
+	}
+	else if (city === "Sydney" || "SYD") {
+		$('body').css('.sydney');
+	};
